546b7edf40dda6d314b94f8ae2539fb759c3e53c
최웅석 <cus4703@gmail.com>
2022-01-21 23:39:18 +0900

    refactor: dto 구조 변경

--- src/main/java/cus/study/security/common/CommonResponse.java
+++ src/main/java/cus/study/security/common/CommonResponse.java
@@ -2,26 +2,38 @@ package cus.study.security.common;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Collections;
 
-public class Response {
+public class CommonResponse {
 
     @Getter
-    @Builder
-    private static class Body {
-        private int state;
+    public static class Body {
+        private int statusCode;
         private String result;
         private String massage;
         private Object data;
         private Object error;
+
+        protected Body() {
+        }
+
+        @Builder
+        public Body(int statusCode, String result, String massage, Object data, Object error) {
+            this.statusCode = statusCode;
+            this.result = result;
+            this.massage = massage;
+            this.data = data;
+            this.error = error;
+        }
     }
 
     public static ResponseEntity<?> success(Object data, String msg, HttpStatus status) {
         Body body = Body.builder()
-                .state(status.value())
+                .statusCode(status.value())
                 .data(data)
                 .result("success")
                 .massage(msg)
@@ -32,7 +44,7 @@ public class Response {
 
     public static ResponseEntity<?> success(Object data, HttpStatus status) {
         Body body = Body.builder()
-                .state(status.value())
+                .statusCode(status.value())
                 .data(data)
                 .result("success")
                 .error(Collections.emptyList())
@@ -44,9 +56,13 @@ public class Response {
         return success(data, null, HttpStatus.OK);
     }
 
+    public static ResponseEntity<?> success() {
+        return success(null, null, HttpStatus.OK);
+    }
+
     public static ResponseEntity<?> fail(Object data, String msg, HttpStatus status) {
         Body body = Body.builder()
-                .state(status.value())
+                .statusCode(status.value())
                 .data(data)
                 .result("fail")
                 .massage(msg)
